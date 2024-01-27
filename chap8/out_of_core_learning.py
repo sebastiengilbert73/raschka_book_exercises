dffc593df078847f4d7311f2527cd307e052b4af
Sebastien Gilbert <sebastiengilbert73@yahoo.ca>
2024-01-27 11:11:19 -0500

    .\out_of_core_learning.py: Trains with minibatches

--- chap8/out_of_core_learning.py
+++ chap8/out_of_core_learning.py
@@ -4,6 +4,9 @@ import numpy as np
 import re
 from nltk.corpus import stopwords
 stop = stopwords.words('english')
+from sklearn.feature_extraction.text import HashingVectorizer
+from sklearn.linear_model import SGDClassifier
+import pyprind
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)-15s %(levelname)s %(message)s')
 
@@ -21,8 +24,42 @@ def stream_docs(path):
             text, label = line[:-3], int(line[-2])
             yield text, label
 
+def get_minibatch(doc_stream, size):
+    docs, y = [], []
+    try:
+        for _ in range(size):
+            text, label = next(doc_stream)
+            docs.append(text)
+            y.append(label)
+    except StopIteration:
+        return None, None
+    return docs, y
+
 def main():
     logging.info(f"out_of_core_learning.main()")
+
+    vect = HashingVectorizer(decode_error='ignore',
+                             n_features=2**21,
+                             preprocessor=None,
+                             tokenizer=tokenizer)
+    clf = SGDClassifier(loss='log_loss', random_state=1)
+    doc_stream = stream_docs(path='./movie_data.csv')
+
+    # Out-of-core learning
+    pbar = pyprind.ProgBar(45)
+    classes = np.array([0, 1])
+    for _ in range(45):
+        X_train, y_train = get_minibatch(doc_stream, size=1000)
+        if not X_train:
+            break
+        X_train = vect.transform(X_train)
+        clf.partial_fit(X_train, y_train, classes=classes)
+        pbar.update()
+
+    # Evaluate
+    X_test, y_test = get_minibatch(doc_stream, size=5000)
+    X_test = vect.transform(X_test)
+    logging.info(f"Accuracy: {clf.score(X_test, y_test):.3f}")
 
 
 if __name__ == '__main__':
